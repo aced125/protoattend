30b562e9959b3dc4671eb69491bd53ce44840ed6
Laksh <laksh@genei.io>
2020-05-28 01:43:22 +0100

    Bump version: 0.1.0 → 0.1.1

--- protoattend/__init__.py
+++ protoattend/__init__.py
@@ -2,7 +2,7 @@
 # fmt: off
 __author__ = """Laksh Aithani"""
 __email__ = 'lakshaithanii@gmail.com'
-__version__ = '__version__ = '0.1.0''
+__version__ = '0.1.1'
 # fmt: on
 
 
--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/aced125/protoattend',
-    version='version='0.1.0'',
+    version='0.1.1',
     zip_safe=False,
 )
 # fmt: on
