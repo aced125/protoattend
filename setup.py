c4e209a54eee99d8aa6357657ed7164547a40e5d
Laksh <laksh@genei.io>
2020-05-28 01:37:10 +0100

    Bump version: 0.1.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/aced125/protoattend',
-    version='0.1.0',
+    version='version='0.1.0'',
     zip_safe=False,
 )
 # fmt: on
